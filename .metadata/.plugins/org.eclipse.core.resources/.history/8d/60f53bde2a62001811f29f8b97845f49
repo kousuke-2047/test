965c1f3014d5c3f4b6cc5d0bfff870562b7c5bd4
kousuke-2047 <black3dar.fe@docomo.ne.jp>
2018-05-29 12:03:36 +0900

    5/29hiru

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8d/60f53bde2a62001811f29f8b97845f49
@@ -0,0 +1,10 @@
+public class Capsule {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int z = x+y;
+		String ans ="x+yは";
+		System.out.println(ans+x+y);
+
+	}
+}
--- Capsule/src/Capsule.java
+++ Capsule/src/Capsule.java
@@ -2,9 +2,9 @@ public class Capsule {
 	public static void main(String[] args) {
 		int x = 5;
 		int y = 10;
-		int z = x+y;
 		String ans ="x+yは";
-		System.out.println(ans+x+y);
+		System.out.print(ans);
+		System.out.print(x+y);
 
 	}
 }
